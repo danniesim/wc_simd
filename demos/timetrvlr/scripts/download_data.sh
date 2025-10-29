d1dc17d8af6d67db858cf29d6efc76c2b303887a
Dannie Sim <dannie.sim@gmail.com>
2025-10-29 13:48:09 +0000

    fix: correct file path in download_data.sh script

--- demos/timetrvlr/scripts/download_data.sh
+++ demos/timetrvlr/scripts/download_data.sh
@@ -3,7 +3,7 @@
 files=( 
     "demos/timetrvlr/public/iiif_no_text_embedding_index.json" 
     "demos/timetrvlr/public/iiif_no_text_embedding_matrix_vlm_embed_ae3d_hires_1.npy"
-    "runs/vlm_embed_vae3d_hires_1/ae.pt"
+    "runs/vlm_embed_ae3d_hires_1/ae.pt"
 )
 
 # activate root virtual environment
